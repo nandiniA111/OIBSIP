9ef3930791332018413f2e3707acb1cf0e4eb2bd
nandiniA111 <124701500+nandiniA111@users.noreply.github.com>
2023-03-05 17:24:19 +0530

    Update index.js

--- Task3/index.js
+++ Task3/index.js
@@ -8,7 +8,6 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
-// Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -17,7 +16,7 @@ for (i = 0; i < close.length; i++) {
     div.style.display = "none";
   }
 }
-// Add a "checked" symbol when clicking on a list item
+
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
@@ -25,8 +24,6 @@ list.addEventListener('click', function(ev) {
   }
 }, false);
 
-
-// Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
